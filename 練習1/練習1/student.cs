71c77aefc7a2a07665a0d5b9c2e561640b29c037
Ezenkappa <ily0955561870@gmail.com>
2020-07-13 03:15:08 +0800

    7/13 C#

---
+++ 練習1/練習1/student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 練習1
+{
+    class student
+    {
+        public string Name;
+        public int Grade;
+
+        public string Talk(int sgrade, string sname)
+        {
+            return Grade + "年級的" + Name + "呼叫" + sgrade + "年級的" +sname;
+        }
+    }
+}
